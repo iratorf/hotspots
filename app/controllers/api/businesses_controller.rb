4aa5f648f7fe7ab02ec90f81dc6e3a3a2c1f2a93
Ira Torf <i.a.torf@gmail.com>
2014-10-02 20:46:29 -0700

    performance

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -20,11 +20,11 @@ module Api
       if params[:distance]
         distance = params[:distance].sort[-1]
         #geolocator
-        # businesses = businesses.near(request.location, distance)
+        businesses = businesses.near(request.location, distance)
         #LS
         # businesses = businesses.near([33.984631, -118.276062], distance)
         #SF
-        businesses = businesses.near([37.791209, -122.415762], distance)
+        # businesses = businesses.near([37.791209, -122.415762], distance)
       end
       @businesses = businesses
 
